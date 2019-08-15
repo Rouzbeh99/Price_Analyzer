699af70341130fbc3b3e7aa9cf95c101474daad6
Rouzbeh99 <ngc1977nebula@yahoo.com>
2019-08-15 22:37:15 +0430

    data added to database successfully.

--- infos.py
+++ infos.py
@@ -7,12 +7,13 @@ connection = mysql.connector.connect(user='Rouzbeh', password='2650236000_',
                                      host='127.0.0.1',
                                      database='test')
 curser = connection.cursor()
-curser.execute('CREATE TABLE IF NOT EXISTS Cars(Brand varchar(30),Model varchar(30),Price integer ,Distance integer )')
+curser.execute(
+    'CREATE TABLE IF NOT EXISTS Cars(Brand varchar(30) not null ,Model varchar(30) not null ,Price integer not null ,Distance integer not null )')
 
 page_number = 0
 numberOfpages = 2
 for i in range(numberOfpages):
-    page_number += 1
+    # page_number += 1
     page = requests.get(
         'https://bama.ir/car/all-brands/all-models/all-trims?page=' + str(page_number))
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -25,12 +26,12 @@ for i in range(numberOfpages):
         inner_soup = BeautifulSoup(inner_page.text, 'html.parser')
         price = inner_soup.find('span', attrs={'itemprop': 'price'}).contents[0]
         price = re.sub(',', '', price)
-        if 'توافقی' in price or 'در توضیحات ' in price:
+        try:
+            price = int(price)
+        except:
             continue
         brand = inner_soup.find('span', attrs={'itemprop': 'brand'}).contents[0]
         model = inner_soup.find('span', attrs={'itemprop': 'model'}).contents[0]
-        price = inner_soup.find('span', attrs={'itemprop': 'price'}).contents[0]
-        price = re.sub(',', '', price)
         distance = 0
         all_infoes = inner_soup.findAll('span')
         for infoe in all_infoes:
@@ -45,9 +46,18 @@ for i in range(numberOfpages):
                 except:
                     distance = 0
                     break
-        query = 'INSERT INTO CARS VALUES(brand,model,price,distance) WHERE NOT EXITS(SELECT * FROM CARS WHERE (Brand = brand AND Model = model AND Price = price AND Distance = distance));'
+        query = 'INSERT INTO CARS VALUE('
+        query += '\''+brand+'\''
+        query += ','
+        query += '\''+model+'\''
+        query += ','
+        query += str(price)
+        query += ','
+        query += str(distance)
+        query += ');'
+
         curser.execute(query)
-        print("car with brand = %s       model = %s        distance = %s       price = %s added to database" % (
+        print("car with brand = %s       model = %s        distance = %d       price = %d added to database" % (
             brand, model, distance, price))
 
 connection.commit()
